5d749335c63871bdf0383d80ac965b9fbad28f89
andrino <earlbrandx@gmail.com>
2024-08-27 22:53:19 +0800

    ako changes

---
+++ app/src/main/java/com/capstone/peopleconnect/Client/ClientViewPager.kt
@@ -0,0 +1,12 @@
+package com.capstone.peopleconnect.Client
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstone.peopleconnect.R
+
+class ClientViewPager : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_client_view_pager)
+    }
+}
